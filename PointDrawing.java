3492a640ee33d96dce97e5164da29ffdcc59f9d1
garbage1010 <74822693+garbage1010@users.noreply.github.com>
2024-04-24 09:44:42 -0500

    Update PointDrawing.java

--- PointDrawing.java
+++ PointDrawing.java
@@ -13,8 +13,21 @@ class Surface extends JPanel implements ActionListener{
   public Surface(){
     initTimer();
   }
-  private initTimer(){
+
+  public Timer getTimer(){
+    return timer;
+  }
+  private void doDrawing(Graphics g){
     
+    Graphics2D g2d = (Graphics2D) g;
+
+    g2d.setPaint(Color.blue);
+
+    int w = getWidth();
+    
+  }
+  private initTimer(){
+    // A javax.swing.Timer is used to create animation. It fires ActionEvents at the specified interval (DELAY).
       timer = new Timer(DELAY, this);
       timer.start();
   }
