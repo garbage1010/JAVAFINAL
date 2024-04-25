5160d406d6859cba63b00d72ad42a1457ffb4aeb
RoryM0 <rory5827@outlook.com>
2024-04-24 21:04:25 -0500

    Updated Level1.java and moved
    moved level1.java into a subfolder. Also I added a label for the background image

--- levels/Level1.java
+++ levels/Level1.java
@@ -1,12 +1,20 @@
 /*
  * "This creates the class for the first level of the game"
  */
-
+package levels;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class Level1 extends JFrame{
     Level1(){
+        //background image
+        ImageIcon questionmarkbackground = new ImageIcon("");
+        JLabel bgimg = new JLabel();
+        bgimg.setIcon(questionmarkbackground);
+        bgimg.setVerticalAlignment(JLabel.CENTER);
+        bgimg.setHorizontalAlignment(JLabel.CENTER);
+
         JFrame scene1 = new JFrame();
         scene1.setTitle("Pre-Calc Panic: Integration by Parts");
         scene1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
