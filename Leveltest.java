7f69f39f13041f49e3a522b21607fbb51eaee754
RoryM0 <rory5827@outlook.com>
2024-04-26 16:43:42 -0500

    Moved folders
    This is because I'm too lazy to figure out how packages work right now

--- Leveltest.java
+++
@@ -1,8 +0,0 @@
-public class Leveltest
-{
-   public static void main(String[]args)
-   {
-      Level1 level = new Level1();
-   }
-
-}
---
+++ levels/Leveltest.java
@@ -0,0 +1,8 @@
+public class Leveltest
+{
+   public static void main(String[]args)
+   {
+      Level1 level = new Level1();
+   }
+
+}
