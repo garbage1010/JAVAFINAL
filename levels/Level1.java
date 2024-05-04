2364b759b97eaf7517dd0df20c8a0b534f3f644e
RoryM0 <rory5827@outlook.com>
2024-05-04 13:33:14 -0500

    Update Level1.java

--- levels/Level1.java
+++ levels/Level1.java
@@ -1,29 +1,48 @@
-
 /*
  * "This creates the class for the first level of the game"
  */
 package levels;
+
+// Importing necessary libraries
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-import java.awt.*;
-
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import javafx.event.ActionEvent;
 
-public class Level1 extends JFrame{
+// Class definition
+public class Level1 extends JFrame implements ActionListener{
+
+    // Creating a button
+    JButton textbox = new JButton();
+
+    // Constructor
     Level1(){
-        //background image
+
+        // Setting button dimensions
+        textbox.setBounds(0, 500, 200, 100);
+        
+        // Background image
         ImageIcon questionmarkbackground = new ImageIcon("textbox.png");
 
-        //background made using jlabel
+        // Creating a JLabel for background image
         JLabel bgimg = new JLabel();
         bgimg.setIcon(questionmarkbackground);
 
+        // Creating and setting up the main frame
         JFrame scene1 = new JFrame();
         scene1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         scene1.setSize(800, 600);
         scene1.setVisible(true);
         scene1.add(bgimg);
-
     }
-}
+
+    // This method is triggered when a button is pressed
+    public void actionPerformed(ActionEvent e) {
+        // Checks if the button pressed is the textbox button
+        if(e.getSource()==textbox){
+            // Action to be performed when the button is pressed
+        }
+    }
