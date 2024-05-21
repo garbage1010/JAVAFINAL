bb88e15e9b65b509ca86a90574851a5b1c059049
RoryM0 <rory5827@outlook.com>
2024-05-20 23:56:19 -0500

    Update Level1.java

--- levels/Level1.java
+++ levels/Level1.java
@@ -1,148 +1,103 @@
-/*
- * "This creates the class for the first level of the game"
- */
-
-
-// Importing necessary libraries
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.Timer;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.awt.event.ActionEvent;
 
-
-// Class definition
-public class Level1 extends JFrame implements ActionListener{
+public class Level1 extends JFrame implements ActionListener {
 
     int framecounter = 1;
     int textcounter = 1;
 
     Timer timer;
 
-    // Creating a button
-    TextFileReader reader =new TextFileReader("levels\\images\\texts\\lv1dialogue.txt", 0, 600, 800, 200);
+    // Assuming TextFileReader is a valid class
+    TextFileReader reader = new TextFileReader("levels\\images\\texts\\lv1dialogue.txt", 0, 600, 800, 200);
 
-    //images used
-    Image frame1 = Toolkit.getDefaultToolkit().getImage("levels\\images\\1\\Frame1-1.PNG"); 
+    // Images used
+    Image frame1 = Toolkit.getDefaultToolkit().getImage("levels\\images\\1\\Frame1-1.PNG");
     Image frame2 = Toolkit.getDefaultToolkit().getImage("levels\\images\\1\\Frame1-2-1.PNG");
     Image frame3 = Toolkit.getDefaultToolkit().getImage("levels\\images\\1\\Frame1-2-2.PNG");
     Image frame4 = Toolkit.getDefaultToolkit().getImage("levels\\images\\1\\Frame1-2-3.png");
     Image frame5 = Toolkit.getDefaultToolkit().getImage("levels\\images\\1\\Frame1-3.png");
     Image frame6 = Toolkit.getDefaultToolkit().getImage("levels\\images\\1\\Frame1-4.png");
 
-
-    JLabel bg = new JLabel(); //Label to be used as background
-
+    JLabel bg = new JLabel(); // Label to be used as background
     JButton errorbutton = new JButton();
 
-    JFrame scene1 = new JFrame();
     // Constructor
-    public Level1(){
+    public Level1() {
+        // Setup the main frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 800);
+        setLayout(null);
+        setVisible(true);
+        setResizable(false);
 
-        //errorbutton.setOpaque(false);
-
-        // Creating a JLabel for background image
+        // Setup background label
         bg.setBounds(0, 0, 800, 600);
         bg.setIcon(new ImageIcon(frame1));
+        add(bg);
 
+        // Add the reader button
+        add(reader.getButton());
 
-        // Creating and setting up the main frame
-
-        scene1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        scene1.setSize(800, 800);
-        scene1.setLayout(null);
-        scene1.setVisible(true);
-        scene1.setResizable(false);
-        scene1.add(bg);
-        scene1.add(reader.getButton());
-        startPolling(); //starts update checking
-
-        //scene1.add(textbox);
+        // Start the timer
+        startPolling();
     }
+
     private void startPolling() {
         timer = new Timer(500, e -> {
             try {
-                int currentValue = reader.currentIndex;
-                switch(reader.currentIndex){
+                int currentIndex = reader.currentIndex;
+                switch (currentIndex) {
                     case 4:
-                        System.out.println(reader.currentIndex);
-                        timer.stop();
-                        reader.setProgress(false);
-                        bg.setIcon(new ImageIcon(frame2));
-                        scene1.add(errorbutton);
-                        errorbutton.setBounds(385, 240, 30, 30);
-                        errorbutton.addActionListener(l -> {
-                            reader.setProgress(true);
-                            reader.currentIndex++;
-                            reader.updateLabel();
-                            scene1.remove(errorbutton);
-                            timer.start();
-                        });
-                        System.out.println(reader.currentIndex);
+                        updateFrame(reader, frame2, 385, 240, 30, 30, 5);
                         break;
                     case 5:
-                        System.out.println(reader.currentIndex);
-                        timer.stop();
-                        reader.setProgress(false);
-                        bg.setIcon(new ImageIcon(frame3));
-                        scene1.add(errorbutton);
-                        errorbutton.setBounds(449, 351, 30, 30);
-                        errorbutton.addActionListener(l -> {
-                            reader.setProgress(true);
-                            reader.currentIndex = 6;
-                            reader.updateLabel();
-                            scene1.remove(errorbutton);
-                            timer.start();
-                        });
-                        System.out.println(reader.currentIndex);
+                        updateFrame(reader, frame3, 449, 351, 30, 30, 6);
                         break;
                     case 7:
-                        System.out.println(reader.currentIndex);
-                        timer.stop();
-                        reader.setProgress(false);
-                        bg.setIcon(new ImageIcon(frame4));
-                        scene1.add(errorbutton);
-                        errorbutton.setBounds(441, 237, 50, 50);
-                        errorbutton.addActionListener(l -> {
-                            reader.setProgress(true);
-                            ++reader.currentIndex;
-                            reader.updateLabel();
-                            scene1.remove(errorbutton);
-                            timer.start();
-                        });
-                        System.out.println(reader.currentIndex);
+                        updateFrame(reader, frame4, 441, 237, 50, 50, 8);
                         break;
                     case 8:
-                        System.out.println(reader.currentIndex);
-                        timer.stop();
-                        reader.setProgress(false);
                         bg.setIcon(new ImageIcon(frame5));
-
+                        timer.stop();
                         break;
                     case 9:
-                        System.out.println(reader.currentIndex);
-                        timer.stop();
-                        reader.setProgress(false);
                         bg.setIcon(new ImageIcon(frame6));
+                        timer.stop();
                         break;
-                            
                     default:
                         System.out.println(reader.currentIndex);
                 }
-                
             } catch (Error er) {
                 System.out.println(er);
             }
         });
         timer.start();
     }
-    // This method is triggered when a button is pressed
+
+    private void updateFrame(TextFileReader reader, Image frame, int x, int y, int width, int height, int nextIndex) {
+        timer.stop();
+        reader.setProgress(false);
+        bg.setIcon(new ImageIcon(frame));
+        add(errorbutton);
+        errorbutton.setBounds(x, y, width, height);
+        errorbutton.addActionListener(l -> {
+            reader.setProgress(true);
+            reader.currentIndex = nextIndex;
+            reader.updateLabel();
+            remove(errorbutton);
+            timer.start();
+        });
+    }
+
     @Override
-    public void actionPerformed(ActionEvent e) { 
-    
+    public void actionPerformed(ActionEvent e) {
+        // Handle button actions if necessary
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Level1());
     }
 }
