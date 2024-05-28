d70072f37b8d0048babc5b660b9bb26cd3ec1ade
RoryM0 <rory5827@outlook.com>
2024-05-28 18:02:39 -0500

    Update Level15.java

--- levels/Level15.java
+++ levels/Level15.java
@@ -4,10 +4,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class Level15 extends JFrame implements ActionListener {
+
+    Image frame1 = Toolkit.getDefaultToolkit().getImage("levels\\images\\2\\Frame4-1.PNG");
     
     public Level15(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 800);
+        setSize(430, 564);
         setLayout(null);
         setVisible(true);
         setResizable(false);
