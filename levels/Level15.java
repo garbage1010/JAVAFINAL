a82872a55e1f1660b66abbce7fcb60ca0f13e032
RoryM0 <rory5827@outlook.com>
2024-06-02 00:52:10 -0500

    Update Level15.java

--- levels/Level15.java
+++ levels/Level15.java
@@ -1,16 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Level15 extends JFrame implements ActionListener {
+public class Level15 extends JFrame implements ActionListener, KeyListener {
 
     Image frame1 = Toolkit.getDefaultToolkit().getImage("levels\\images\\15\\Frame3-1redo.PNG");
-    Image Sprite = Toolkit.getDefaultToolkit().getImage("levels\\images\\15\\Sprite.PNG");
-
+    Image sprite = Toolkit.getDefaultToolkit().getImage("levels\\images\\15\\Sprite.PNG");
     
     JLabel bg = new JLabel();
-    
+    int spriteX = 200;  // Initial X position of the sprite
+    int spriteY = 200;  // Initial Y position of the sprite
+    boolean movementAllowed = true; // Variable to control movement
     
     public Level15(){
         bg.setIcon(new ImageIcon(frame1));
@@ -22,15 +25,55 @@ public class Level15 extends JFrame implements ActionListener {
         setVisible(true);
         setResizable(false);
         add(bg);
+        
+        addKeyListener(this);
+        setFocusable(true);
     }
     
-    public static void main(String[]args){
-        new Level15();
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(sprite, spriteX, spriteY, this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (movementAllowed) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_W:
+                    spriteY -= 5;
+                    break;
+                case KeyEvent.VK_A:
+                    spriteX -= 5;
+                    break;
+                case KeyEvent.VK_S:
+                    spriteY += 5;
+                    break;
+                case KeyEvent.VK_D:
+                    spriteX += 5;
+                    break;
+            }
+            repaint();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public static void main(String[] args){
+        new Level15();
+    }
 }
