353998fb8e2d4e85697be899ad91458dac3e6e13
RoryM0 <rory5827@outlook.com>
2024-06-02 15:32:38 -0500

    Update Level15.java

--- levels/Level15.java
+++ levels/Level15.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
 
 public class Level15 extends JFrame implements ActionListener, KeyListener {
 
@@ -14,7 +15,8 @@ public class Level15 extends JFrame implements ActionListener, KeyListener {
     int spriteX = 200;  // Initial X position of the sprite
     int spriteY = 200;  // Initial Y position of the sprite
     boolean movementAllowed = true; // Variable to control movement
-    
+    double angle = 0; // Angle to rotate the sprite
+
     public Level15(){
         bg.setIcon(new ImageIcon(frame1));
         bg.setBounds(0,0,430,564);
@@ -34,7 +36,11 @@ public class Level15 extends JFrame implements ActionListener, KeyListener {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(sprite, spriteX, spriteY, this);
+        AffineTransform transform = new AffineTransform();
+        transform.translate(spriteX + sprite.getWidth(this) / 2, spriteY + sprite.getHeight(this) / 2);
+        transform.rotate(Math.toRadians(angle));
+        transform.translate(-sprite.getWidth(this) / 2, -sprite.getHeight(this) / 2);
+        g2d.drawImage(sprite, transform, this);
     }
 
     @Override
@@ -54,15 +60,19 @@ public class Level15 extends JFrame implements ActionListener, KeyListener {
             switch (key) {
                 case KeyEvent.VK_W:
                     spriteY -= 5;
+                    angle = 0;
                     break;
                 case KeyEvent.VK_A:
                     spriteX -= 5;
+                    angle = -90;
                     break;
                 case KeyEvent.VK_S:
                     spriteY += 5;
+                    angle = 180;
                     break;
                 case KeyEvent.VK_D:
                     spriteX += 5;
+                    angle = 90;
                     break;
             }
             repaint();
