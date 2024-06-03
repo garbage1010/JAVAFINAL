f94c21284a73566a2d74ceb56e0e3ca21bde024a
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2024-06-02 21:15:48 -0500

    Update GameSaveData.java

--- GameSaveData.java
+++ GameSaveData.java
@@ -1,55 +1,21 @@
-import java.io.*;
+import java.io.Serializable;
 
-// This class represents the game save data
-class GameSaveData implements Serializable {
-    private int currLevel;
-    
+public class GameSaveData implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int playerLevel;
+
     // Constructor
-    public GameSaveData(int level, int score) {
+    public GameSaveData(int level) {
         this.playerLevel = level;
-        this.playerScore = score;
     }
-    
+
     // Getter and setter methods
-    public int getCurrLevel() {
-        return currLevel;
+    public int getPlayerLevel() {
+        return playerLevel;
     }
 
-    public void setCurrLevel(int currLevel) {
-        this.currLevel = currLevel;
-    }
-
-// This class handles saving and loading the game data
-class GameSaveManager {
-    private static final String SAVE_FILE_PATH = "savedata.dat";
-    
-    // Save game data to a file
-    public static void saveGame(GameSaveData data) {
-        try {
-            FileOutputStream fileOut = new FileOutputStream(SAVE_FILE_PATH);
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-            objectOut.writeObject(data);
-            objectOut.close();
-            fileOut.close();
-            System.out.println("Game data saved successfully.");
-        } catch (IOException e) {
-            System.err.println("Error saving game data: " + e.getMessage());
-        }
-    }
-    
-    // Load game data from a file
-    public static GameSaveData loadGame() {
-        GameSaveData data = null;
-        try {
-            FileInputStream fileIn = new FileInputStream(SAVE_FILE_PATH);
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-            data = (GameSaveData) objectIn.readObject();
-            objectIn.close();
-            fileIn.close();
-            System.out.println("Game data loaded successfully.");
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Error loading game data: " + e.getMessage());
-        }
-        return data;
+    public void setPlayerLevel(int playerLevel) {
+        this.playerLevel = playerLevel;
     }
 }
