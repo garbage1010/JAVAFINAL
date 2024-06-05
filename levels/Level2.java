997ab4d2721f4e1b46541d1b4a66e12ab6758f19
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2024-06-04 19:27:07 -0500

    Update Level2.java

--- levels/Level2.java
+++ levels/Level2.java
@@ -47,6 +47,32 @@ public class Level2 extends JFrame implements ActionListener {
         scene.add(cipher);
 
         // Start the background music
+           public static void playBackgroundMusic(String filePath) {
+        try {
+            // Open an audio input stream
+            File musicFile = new File(filePath);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(musicFile);
+
+            // Get a sound clip resource
+            Clip clip = AudioSystem.getClip();
+
+            // Open audio clip and load samples from the audio input stream
+            clip.open(audioStream);
+
+            // Start the clip in a loop
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+            // Start playing the music
+            clip.start();
+
+            // Keep the program running to play the music
+            Thread.sleep(clip.getMicrosecondLength() / 1000);
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
         playBackgroundMusic("levels\\images\\texts\\lvl2ambience.mp3");
 
         startPolling();
