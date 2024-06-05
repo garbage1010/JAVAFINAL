a62f728de4a3315262b5cffd10b2fe2f3b45ff03
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2024-06-04 19:28:02 -0500

    Update Level4.java

--- Level4.java
+++ Level4.java
@@ -44,7 +44,34 @@ public class Level4 extends JFrame implements ActionListener {
         add(reader.getButton());
 
         // Start the background music
-        startBackgroundMusic("levels\\images\\texts\\lvl4ambience.mp3");
+        // Start the background music
+           public static void playBackgroundMusic(String filePath) {
+        try {
+            // Open an audio input stream
+            File musicFile = new File(filePath);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(musicFile);
+
+            // Get a sound clip resource
+            Clip clip = AudioSystem.getClip();
+
+            // Open audio clip and load samples from the audio input stream
+            clip.open(audioStream);
+
+            // Start the clip in a loop
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+            // Start playing the music
+            clip.start();
+
+            // Keep the program running to play the music
+            Thread.sleep(clip.getMicrosecondLength() / 1000);
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+        playBackgroundMusic("levels\\images\\texts\\lvl4ambience.mp3");
 
         // Start the timer
         startPolling();
