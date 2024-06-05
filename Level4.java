eb14a9ea733ec3fa51e912335cb3cd68e9787c1a
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2024-06-04 19:21:33 -0500

    Update Level4.java

--- Level4.java
+++ Level4.java
@@ -50,26 +50,6 @@ public class Level4 extends JFrame implements ActionListener {
         startPolling();
     }
 
-    private void startBackgroundMusic(String filepath) {
-        try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(WavPlayer.class.getResourceAsStream(filePath));
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-
-            clip.addLineListener(event -> {
-                if (event.getType() == LineEvent.Type.STOP) {
-                    clip.setMicrosecondPosition(0); // Reset position to the beginning
-                    clip.start(); // Restart playback
-                }
-            });
-
-            clip.start();
-            Thread.sleep(Long.MAX_VALUE); // Keep the thread alive to allow continuous playback
-        } catch (Exception e) {
-            System.err.println("Error playing sound: " + e.getMessage());
-        }
-    }
-
     private void startPolling() {
         timer = new Timer(500, e -> {
             try {
