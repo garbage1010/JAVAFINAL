b9f2b07e2c9529bd1416aa48921629e34c9db14d
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2024-06-05 22:19:50 -0500

    Update Level4.java

--- levels/Level4.java
+++ levels/Level4.java
@@ -8,10 +8,7 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.File;
-import javax.sound.sampled.*;
-import java.io.FileInputStream;
 import java.io.IOException;
-
 
 public class Level4 extends JFrame implements ActionListener {
 
@@ -20,19 +17,16 @@ public class Level4 extends JFrame implements ActionListener {
     TextFileReader reader = new TextFileReader("levels\\images\\texts\\lv4dialogue.txt", 0, 600, 800, 200);
 
     // Images used
-    Image frame1 = Toolkit.getDefaultToolkit().getImage("levels\\images\\4\\Final1.PNG");
-    Image frame2 = Toolkit.getDefaultToolkit().getImage("levels\\images\\4\\Final2.PNG");
-    Image frame3 = Toolkit.getDefaultToolkit().getImage("levels\\images\\4\\Final3.PNG");
-    Image frame4 = Toolkit.getDefaultToolkit().getImage("levels\\images\\4\\Final4.PNG");
-    Image frame5 = Toolkit.getDefaultToolkit().getImage("levels\\images\\4\\Final5.PNG");
-    Image frame6 = Toolkit.getDefaultToolkit().getImage("levels\\images\\4\\Final6.PNG");
-    Image frame7 = Toolkit.getDefaultToolkit().getImage("levels\\images\\4\\Goodgoodend.PNG"); 
+    ImageIcon frame1 = new ImageIcon("levels\\images\\4\\Final1.PNG");
+    ImageIcon frame2 = new ImageIcon("levels\\images\\4\\Final2.PNG");
+    ImageIcon frame3 = new ImageIcon("levels\\images\\4\\Final3.PNG");
+    ImageIcon frame4 = new ImageIcon("levels\\images\\4\\Final4.PNG");
+    ImageIcon frame5 = new ImageIcon("levels\\images\\4\\Final5.PNG");
+    ImageIcon frame6 = new ImageIcon("levels\\images\\4\\Final6.PNG");
+    ImageIcon frame7 = new ImageIcon("levels\\images\\4\\Goodgoodend.PNG");
 
     JLabel bg = new JLabel(); // Label to be used as background
-    //new FadeOutOverlay(bg); 
     JButton selectSyringe;
-    JButton errorbutton = new JButton(); // Assuming this button is needed
-    String filePath = "levels\\images\\texts\\lvl4ambience.wav";
 
     // Constructor
     public Level4() {
@@ -45,7 +39,7 @@ public class Level4 extends JFrame implements ActionListener {
 
         // Setup background label
         bg.setBounds(0, 0, 800, 600);
-        bg.setIcon(new ImageIcon(frame1));
+        bg.setIcon(frame1);
         add(bg);
 
         // Add the reader button
@@ -58,29 +52,6 @@ public class Level4 extends JFrame implements ActionListener {
         startPolling();
     }
 
-    public static void playBackgroundMusic(String filePath) {
-        try {
-            // Open an audio input stream
-            File musicFile = new File(filePath);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(musicFile);
-
-            // Get a sound clip resource
-            Clip clip = AudioSystem.getClip();
-
-            // Open audio clip and load samples from the audio input stream
-            clip.open(audioStream);
-
-            // Start the clip in a loop
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-
-            // Start playing the music
-            clip.start();
-
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
-    }
-
     private void startPolling() {
         timer = new Timer(500, e -> {
             try {
@@ -88,31 +59,33 @@ public class Level4 extends JFrame implements ActionListener {
                 System.out.println("Current Index: " + currentIndex); // Debug print
                 switch (currentIndex) {
                     case 2:
-                        bg.setIcon(new ImageIcon(frame2));
+                        bg.setIcon(frame2);
                         break;
                     case 3:
-                        bg.setIcon(new ImageIcon(frame3));
+                        bg.setIcon(frame3);
                         break;
                     case 4:
-                        bg.setIcon(new ImageIcon(frame4));
+                        bg.setIcon(frame4);
                         break;
                     case 5:
-                        bg.setIcon(new ImageIcon(frame5));
+                        bg.setIcon(frame5);
                         selectSyringe = new JButton();
                         selectSyringe.setFocusable(false);
                         selectSyringe.setBounds(100, 100, 100, 50); // Placeholder bounds
                         selectSyringe.setOpaque(false);
                         add(selectSyringe);
+                        revalidate();
+                        repaint();
                         break;
-                    case 6: 
-                        bg.setIcon(new ImageIcon(frame6)); 
-                        break; 
-                    case 7: 
-                        bg.setIcon(new ImageIcon(frame7)); 
+                    case 6:
+                        bg.setIcon(frame6);
+                        break;
+                    case 7:
+                        bg.setIcon(frame7);
                         PreCalcPanic.increment();
                         SaveThat.saveLevel(5);
-                        dispose(); 
-                        break; 
+                        dispose();
+                        break;
                     default:
                         if (currentIndex < reader.lines.size()) {
                             reader.updateLabel(); // Ensure label updates even in default case
@@ -127,18 +100,19 @@ public class Level4 extends JFrame implements ActionListener {
         timer.start();
     }
 
-    private void updateFrame(TextFileReader reader, Image frame, int x, int y, int width, int height, int nextIndex) {
+    private void updateFrame(TextFileReader reader, ImageIcon frame, int x, int y, int width, int height, int nextIndex) {
         reader.currentIndex = nextIndex;
-        bg.setIcon(new ImageIcon(frame));
+        bg.setIcon(frame);
         errorbutton.setBounds(x, y, width, height);
         add(errorbutton);
+        revalidate();
+        repaint();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         // Handle button actions if necessary
     }
-    
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new Level4());
