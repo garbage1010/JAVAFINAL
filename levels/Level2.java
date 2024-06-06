66adcb1c9f9bc89af5894cca695f08904cc0c826
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2024-06-05 20:18:27 -0500

    Update Level2.java

--- levels/Level2.java
+++ levels/Level2.java
@@ -143,11 +143,9 @@ public class Level2 extends JFrame implements ActionListener {
                         break;
                     case 7:
                         bg.setIcon(new ImageIcon(frame5)); // Final frame after minigame
-                        break;
-                    case 8: 
-                         PreCalcPanic.increment();
-                         SaveThat.saveLevel(3);
-                         System.exit(0);
+                        PreCalcPanic.increment();
+                        SaveThat.saveLevel(3);
+                        System.exit(0);
                     default:
                         break;
                 }
